4d5a72083e61a30b3225a886cd42ca5c75b8b2e2
K Gowtham Kumar <gowthamkandukuri955@gmail.com>
2026-09-22 10:32:23 +0530

    fix api url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,9 +16,9 @@ function App() {
     const fetchPayments = async () => {
         try {
 
-            const response = await fetch(
-                "http://localhost:3000/payments"
-            );
+          const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/payments`
+);
 
             const data = await response.json();
 
